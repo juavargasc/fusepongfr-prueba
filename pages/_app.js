8bdca772d7950e51ca065195fd1be1c56e69e20d
Julian Vargas <julian1245@hotmail.com>
2021-08-19 22:37:33 -0500

    ajuste

--- pages/_app.js
+++ pages/_app.js
@@ -17,8 +17,8 @@ function MyApp({ Component, pageProps }) {
             <MainLayout>
         		<Component {...pageProps}/>
       		</MainLayout>
-      		<script async src="/jquery-3.5.1.slim.min.js" ></script>
-      		<script async src="/bootstrap.min.js" ></script>
+      		<script defer src="/jquery-3.5.1.slim.min.js" ></script>
+      		<script defer src="/bootstrap.min.js" ></script>
         </ContextProvider>
     )
 }
